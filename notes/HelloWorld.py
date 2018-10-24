9a8ed22e740096a19ba605b302a9fdaff4c32019
ANTHONYcpu <44374206+ANTHONYcpu@users.noreply.github.com>
2018-10-24 09:45:42 -0700

    added our first notes

--- notes/HelloWorld.py
+++ notes/HelloWorld.py
@@ -1,1 +1,55 @@
-print
+print("Hello World!")
+
+
+# This is a comment. This has no effect on the code
+# 1. but this does allow me to do things. I can:
+# 2. Comment pieces of code that do not work
+# 3. Make my code easier to read
+
+print ("There should be a couple blank lines here."
+print ()
+print ()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+print (%5 is really old. They are %5 years old." % (name, age)
+
+
+car_name = "Wiebe Mobile"
+car_type = "Tesla"
+car_cylinders = 16
+car_miles_per_gallon = 0.01
+
+# Make it print "I have a car called Wiebe Mobile. It is a Tesla."
+
+
+# Recasting
+real_age = int (input("How old are you again?") )
+hidden_age = real_age + 5
+print ("This is your real age:  %d" % hidden_age)
