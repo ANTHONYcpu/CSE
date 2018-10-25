95d1f4a89a9c398e3b5ee826d74c89f701b105b1
ANTHONYcpu <44374206+ANTHONYcpu@users.noreply.github.com>
2018-10-25 09:43:20 -0700

    Anthony W - Mad Labd

--- notes/HelloWorld.py
+++ notes/HelloWorld.py
@@ -4,41 +4,46 @@ print("Hello World!")
 # This is a comment. This has no effect on the code
 # 1. but this does allow me to do things. I can:
 # 2. Comment pieces of code that do not work
+
+
+
+
+
 # 3. Make my code easier to read
 
-print ("There should be a couple blank lines here."
+print ("There should be a couple blank lines here.")
 print ()
 print ()
+print("There should be a couple blank lines here.")
 
+# Math
+print(3 + 5)
+print(5 - 2)
+print(3 * 4)
+print(6 / 2)
 
+print("Figure this out...")
+print(6 // 2)
+print(5 // 2)
+print(9 // 4)
 
+print("Here is another one...")
+print(6 % 2)
+print(5 % 2)
+print(11 % 4)  # Modulus (Remainder)
 
+# Powers
+# What is 2^100?
+print(2 ** 100)
 
+# Taking input
+name = input("What is your name?")
+print("Hello %s." % name)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-print (%5 is really old. They are %5 years old." % (name, age)
+age = input("How old are you? >_")
+print("%s?!? You belong in a museum." % age)
+print()
+print ("%s is really old. They are %s years old." % (name, age))
 
 
 car_name = "Wiebe Mobile"
