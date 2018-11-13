31ef29afe939cc529823eb8221bd309938fc5377
ANTHONYcpu <44374206+ANTHONYcpu@users.noreply.github.com>
2018-11-13 09:45:12 -0800

    game

--- notes/Anthony W - Guess Game.py
+++ notes/Anthony W - Guess Game.py
@@ -1,2 +1,18 @@
 import random
-r = (random.randint)
+r = (random.randint(1, 10))
+guesses_left = 5
+playing = True
+
+print("Guess a number from 1-10")
+
+while guesses_left > 0 and playing:
+    guess = int(input("Guess="))
+    if guess > r:
+        print ("Lower")
+        guesses_left -= 1
+    elif guess < r:
+        print ("Greater")
+        guesses_left -= 1
+    else:
+        print ("Correct!")
+        playing = False
