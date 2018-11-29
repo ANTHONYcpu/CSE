132dfbaed7151b96d761aadc970970693e78ec02
ANTHONYcpu <44374206+ANTHONYcpu@users.noreply.github.com>
2018-11-29 13:34:53 -0800

    Nice a Clean

--- notes/lucky 7.py
+++ notes/lucky 7.py
@@ -9,4 +9,17 @@ money = 15
 while money > 0:
     Dice_1 = random.randint(1, 6)
     Dice_2 = random.randint(1, 6)
-    if
+    if Dice_1 + Dice_2 == 7:
+        print("You win 4 dollars")
+        round += 1
+        print("Round:", round)
+        money = money + 4
+    else:
+        print("You lose 1 dollar")
+        round += 1
+        print("Round:", round)
+        money -= 1
+
+        if money == 0:
+            print("you ran out of money on Round", round)
+            playing = False 
