0ac1d667e64bc4173c558ab087d4f2a18edfa4c6
ANTHONYcpu <44374206+ANTHONYcpu@users.noreply.github.com>
2018-12-05 13:38:43 -0800

    AntMADLIPS

--- notes/AnthonyW-Madlip.py
+++ notes/AnthonyW-Madlip.py
@@ -1,5 +1,13 @@
-adjective = input("Type in an adjective: ")
-noun = input("Type in a noun: ")
+name = input("type in a name")
+places = input("Type in a place")
+nouns = input("type in an nouns")
+verbs = input("Type in an verbs")
+adjective = input("type in an adjective")
+plural_noun: str = input("type in a plural noun")
 
 
-print("I saw a %s %s" % (adjective, noun)
+print("Ladies and gentleman, on this %s occasion it is a privilege" % adjective)
+print("to address such a %s looking group of %s" % (adjective, plural_noun))
+print("I can tell from your smiling %s that you will support my %s program in the coming election" %
+      (plural_noun, adjective))
+print
