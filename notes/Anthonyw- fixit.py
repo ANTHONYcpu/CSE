d2a83f0e12befd27003444e3b09d6b8ea5289f0f
ANTHONYcpu <44374206+ANTHONYcpu@users.noreply.github.com>
2018-12-10 09:44:50 -0800

    fix it

--- notes/Anthonyw- fixit.py
+++ notes/Anthonyw- fixit.py
@@ -1,7 +1,7 @@
 print("How old are you?",end='')
 age = input()
 print("How tall are you?",end='')
-print("How much do you weigh?",end=' '
+print("How much do you weigh?",end=''
 weight = input()
 
 print("So, you're %s old, %s tall and %s heavy.") % (age, height, weight))
