f51b5c61af32941247e6c674408e3fef8ac9fbe7
ANTHONYcpu <44374206+ANTHONYcpu@users.noreply.github.com>
2019-01-08 09:45:33 -0800

    review notes

---
+++ semester 2 Notes.py
@@ -0,0 +1,22 @@
+print("Hello World")
+
+# This is a single-line commment.
+
+cars = 5
+driving = True
+
+print("I have %d cars" % cars)
+print("I have " + str(cars) + " cars")
+
+age = input("How old are you?")
+print(age + "??? You really shouldn't lie about your age.")
+
+colors = ["blue", "orange", "yellow", "green", "cyan"]
+colors.append("magenta")
+
+print(colors)
+colors.pop(0)
+print(colors)
+
+
+
