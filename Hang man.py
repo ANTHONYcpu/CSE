8b572440154486e7490379ae70d44c6d3fa576cb
ANTHONYcpu <44374206+ANTHONYcpu@users.noreply.github.com>
2019-01-16 09:45:05 -0800

    hman23

--- Hang man.py
+++ Hang man.py
@@ -1,5 +1,9 @@
 import random
-word_bank = ["bango","edit","crypt","fervid","waxy","cat","edison","school","book","notes"]
+word_bank = ["Bango","edit","crypt","fervid","waxy","cat","edison","school","book","notes"]
 
 word = random.choice(word_bank)
 print(word)
+word = []
+
+guesses = 8
+word_bank = guesses
