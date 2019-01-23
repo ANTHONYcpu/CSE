22b46633200d95ccde949c2fcf07e6b628338054
ANTHONYcpu <44374206+ANTHONYcpu@users.noreply.github.com>
2019-01-23 09:44:23 -0800

    hangmen

--- Hang man.py
+++ Hang man.py
@@ -1,9 +1,6 @@
 import random
-word_bank = ["Bango","edit","crypt","fervid","waxy","cat","edison","school","book","notes"]
+word_bank = ["bango","edit","crypt","fervid","waxy","cat","edison","school","book","notes"]
 
 word = random.choice(word_bank)
 print(word)
 letters = []
-
-guesses = 8
-word_bank = guesses
