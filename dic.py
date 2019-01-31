f43f11786ebbacd048d58fbbd712edf5940b1e43
ANTHONYcpu <44374206+ANTHONYcpu@users.noreply.github.com>
2019-01-31 09:45:13 -0800

    commit1

--- dic.py
+++ dic.py
@@ -1,34 +1,179 @@
 states = {
+
     "CA": "California",
-    "FL": "Florida",
-    "AK": "Alaska",
-    "GA": "Georgia"
+
+    "NJ": "New Jersey",
+
+    "WI": "Wisconsin",
+
+    "NY": "New York"
+
 }
+
+
 
 print(states["CA"])
-print(states["AK"])
+
+print(states["WI"])
+
+
 
 nested_dictionary = {
+
     "CA": {
-        "NAME": "Califotnia",
+
+        "NAME": "California",
+
         "POPULATION": 39500000  # 39,500,000
+
     },
-    "FL": {
-        "NAME": "Florida",
-        "POPULATION": 21300000  # 21,300,000
+
+    "NJ": {
+
+        "NAME": "New Jersey",
+
+        "POPULATION": 9000000  # 9,000,000
+
     },
-    "AK": {
-        "NAME": "Alaska",
-        "POPULATION": 737000  # 737,000
+
+    "WI": {
+
+        "NAME": "Wisconsin",
+
+        "POPULATION": 5800000  # 5,800,000
+
     },
-    "GA": {
-        "NAME": "georgia",
-        "POPULATION": 10500000  # 10,500,000
+
+    "NY": {
+
+        "NAME": "New York",
+
+        "POPULATION": 19500000  # 19,500,000
+
     }
+
 }
 
-print(nested_dictionary["GA"]["POPULATION"])
-print(nested_dictionary["FL"]["NAME"])
 
-georgia = nested_dictionary["GA"]
-print(georgia)
+
+print(nested_dictionary["CA"])
+
+print(nested_dictionary["CA"]["POPULATION"])
+
+
+
+complex_dictionary = {
+
+    "CA": {
+
+        "NAME": "California",
+
+        "POPULATION": 39500000,  # 39,500,000
+
+        "CITIES": [
+
+            "Fresno",
+
+            "San Francisco",
+
+            "Los Angeles"
+
+        ]
+
+    },
+
+    "NJ": {
+
+        "NAME": "New Jersey",
+
+        "POPULATION": 9000000,  # 9,000,000
+
+        "CITIES": [
+
+            "Newark",
+
+            "Trenton",
+
+            "Princeton"
+
+        ]
+
+    },
+
+    "WI": {
+
+        "NAME": "Wisconsin",
+
+        "POPULATION": 5800000,  # 5,800,000
+
+        "CITIES": [
+
+            "Madison",
+
+            "Milwaukee",
+
+            "Green Bay"
+
+        ]
+
+    },
+
+    "NY": {
+
+        "NAME": "New York",
+
+        "POPULATION": 19500000,  # 19,500,000
+
+        "CITIES": [
+
+            "New York City",
+
+            "Rockester",
+
+            "Buffalo"
+
+        ]
+
+    }
+
+}
+
+
+
+print(complex_dictionary["NY"]["CITIES"][0])
+
+print(complex_dictionary["NJ"]["CITIES"][2])
+
+
+
+print(complex_dictionary.keys())
+
+print(complex_dictionary.items())
+
+print(nested_dictionary.items())
+
+
+
+for key, value in complex_dictionary.items():
+
+    print(key)
+
+    print(value)
+
+    print("-" * 20)
+
+
+
+print()
+
+for state, info in complex_dictionary.items():
+
+    for title, desc in info.items():
+
+        print(title)
+
+        print(desc)
+
+        print("-" * 20)
+
+    print('=' * 20)
