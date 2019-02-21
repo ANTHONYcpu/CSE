a46410ce4c84582bd1eb39d2d4770c8bbd5aa40a
ANTHONYcpu <44374206+ANTHONYcpu@users.noreply.github.com>
2019-02-21 09:45:09 -0800

    world map boi

--- World map OOP.py
+++ World map OOP.py
@@ -1,20 +1,15 @@
 class Room(object):
-    def __init__(self, name, north=None, south=None, east=None):
+    def __init__(self, name, north=None, south=None, east=None, west=None, description=""):
         self.name = name
         self.north = north
         self.south = south
         self.east = east
+        self.west = west
+        self.description = description
 
 
-# Option 1 - Define as we go
-R19A = Room("Mr. Wiebe's Room")
-parking_lot = Room("Parking Lot", None, R19A)
-
-R19A.north = parking_lot
-
-# Option 2 - Set all at once, modify controller
-R19A = Room("Mr. Wiebe's Room", 'parking_lot')
-parking_lot = Room("parking Lot", None, "R19A")
-
-R19A = Room("NBA2K Playground", 'Playground')
-Playground = Room("Playground", None, "R19A")
+courts = Room("NBA 2K COURTS", 'basketball_courts', 'neighborhood', "store", "clothes_store", "INSERT DESCRIPTION HERE")
+basketball_courts = Room("NBA 2K 3s COURTS, NBA 2K DOTS, NBA 2K 2s COURTS")
+neighborhoods = Room("team practice facility","foot locker store","king of the courts","2K zone","the venue","the downtown courts")
+store = Room("shose","gear","hoildayclothes","headbands","wristbands")
+clothes_store = Room("shirts","pants","shorts","hats","jackets")
