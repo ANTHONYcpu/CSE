8f9ece237e2c5d437f2bc2a427dc0bd44198da5a
ANTHONYcpu <44374206+ANTHONYcpu@users.noreply.github.com>
2019-03-06 09:45:33 -0800

    Item15

--- item.py
+++ item.py
@@ -1,5 +1,8 @@
 class Item(object):
-    def __init__(self, item ):
-        self.item = item
-
-class minis 
+    def __init__(self, name, description):
+        self.name = name
+        self.description = description
+        
+class Mini(Item):
+    def __init__(self):
+        super(Mini, self).__init__("Mini Shield", "Small Shield Potion takes 2 seconds to consume, and upon consumption grants 25 Shield to a maximum of 50")
