7c07fd8c9ec05940214a78b20b39fa7660121b93
ANTHONYcpu <44374206+ANTHONYcpu@users.noreply.github.com>
2019-03-07 09:44:51 -0800

    iitteemm

--- World map OOP.py
+++ World map OOP.py
@@ -12,5 +12,6 @@ courts = Room("NBA 2K COURTS", 'basketball_courts', 'neighborhood', "store", "cl
 basketball_courts = Room("NBA 2K 3s COURTS, NBA 2K DOTS, NBA 2K 2s COURTS")
 neighborhood = Room("team practice facility","foot_locker_store","king_of_the courts","2K zone","the venue","the downtown courts")
 store = Room("Doc's","Wheels","alley_oops tattoo","boost stand","foot_locker_store")
-clothes_store = Room("Swag's","NBA 2K store")
+clothes_store = Room("Swag's","NBA 2K2 store")
 NBA_loading_screen = Room=("play_now","black_top","my_team","freestyle")
+
