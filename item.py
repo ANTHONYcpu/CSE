77339e0e568f96833bd2585d1695661de7297639
ANTHONYcpu <44374206+ANTHONYcpu@users.noreply.github.com>
2019-03-11 09:47:19 -0700

    item12

--- item.py
+++ item.py
@@ -2,12 +2,16 @@ class Item(object):
     def __init__(self, name, description):
         self.name = name
         self.description = description
+
+class Consumable(Item):
+    def __init__(self,name, description, quantity=1):
+        super(Consumable, self).__init__(name)
+        self.description = description
+        self.quantity = quantity
         
-class Mini(Item):
+class Mini(Consumable):
     def __init__(self):
         super(Mini, self).__init__("Mini Shield", "Small Shield Potion takes 2 seconds to consume, and upon consumption grants 25 Shield to a maximum of 50")
         super =("Mini 25 shield")
         def __init__(description):
             super(Mini, description) ("if you drink 1 Mini you would go up 25 shield, if you drink 2 Mini you would go up 50 shield")
-
-class
