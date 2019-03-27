1b1554084a1bf89017bb298b09793c8ee4707d7e
ANTHONYcpu <44374206+ANTHONYcpu@users.noreply.github.com>
2019-03-27 09:45:43 -0700

    gtcw

--- item.py
+++ item.py
@@ -12,31 +12,10 @@ class Mini(Consumable):
     def __init__(self):
         super(Mini, self).__init__("Mini Shield", "Small Shield Potion takes 2 seconds to consume, and upon consumption grants 25 Shield to a maximum of 50")
 
-class Weapon(Item):
-    def __init__(self):
-        super(Weapon, self).__init__("")
-
-
-class Mini_gun(Weapon):
-    def __init__(self, name, description, quantity=1):
-        super(Mini_gun, self).__init__("Mini_gun, It uses Light Bullets. Upon firing the Minigun, there is a short delay before it begins to shoot bullets.")
-
-
-
-
-
-
-
-
-
-class Item(object):
-    def __init__(self, name):
-        self.name = name
-
 
 class Weapon(Item):
-    def __init__(self, name, damage):
-        super(Weapon, self).__init__(name)
+    def __init__(self, name, description, damage):
+        super(Weapon, self).__init__(name, description)
         self.damage = damage
 
 
@@ -52,3 +31,17 @@ class Character(object):
         self.health = health
         self.weapon = weapon
         self.armor = armor
+
+
+class Mini_gun(Weapon):
+    def __init__(self, name, description, quantity=1):
+        super(Mini_gun, self).__init__("Mini_gun", "It uses Light Bullets. Upon firing the Minigun, there is a "
+                                       "short delay before it begins to shoot bullets.", 18)
+
+class HeavyAssaultRifle(Weapon):
+    def __init__(self, name, description, quantity=1):
+        super(HeavyAssaultRifle, self).__init__("HeavyAssaultRifle", "The weapon should be tap-fired for maximum accuracy and damage. It uses Medium Bullets", 48)
+
+class PumpShotgun(Weapon):
+    def __init__(self, name,description, quantity=1):
+        super(PumpShotgun, self).__init__("PumpShotgun", "The it uses Shells 'n' Slugs.", 110 )
