24ae981fded925b912c12a42bbcc9eca1e1dc3e3
ANTHONYcpu <44374206+ANTHONYcpu@users.noreply.github.com>
2019-04-12 10:39:12 -0700

    cmon

--- Anthony W Game.py
+++ Anthony W Game.py
@@ -147,12 +147,21 @@ class TacticalShotGun(Weapon):
         super(TacticalShotGun, self).__init__("TacticalShotGun", "It uses Shells 'n' Slugs", 74)
 
 
-courts = Room("NBA 2K COURTS", 'basketball_courts', 'neighborhood', "store", "clothes_store",)
-basketball_courts = Room("NBA 2K 3s COURTS", "NBA 2K DOTS")
+courts = Room("NBA 2K COURTS", 'basketball_courts', 'neighborhood', "store", "clothes_store")
+basketball_courts = Room("NBA 2K 3s COURTS", "NBA 2K DOTS", "NBA 2K 2s COURTS")
 neighborhood = Room("team practice facility","foot_locker_store","king_of_the courts","2K zone","the venue")
 store = Room("Doc's","Wheels","alley_oops tattoo","boost stand","foot_locker_store")
-clothes_store = Room("Swag's","NBA 2K2 store")
-NBA_loading_screen = Room=("play_now","black_top","my_team","freestyle")
+clothes_store = Room("Swag's","NBA 2K19 store")
+NBA_loading_screen = Room("play_now","black_top","my_team","freestyle")
+Ante_Up = Room("NBA 2K COURTS","basketball_courts")
+DailySpin = Room("Spin_Wheel","DailySpinChoice")
+Cages = Room("NBA 2K COURTS","basketball_courts","NBA 2K DOTS","NBA 2K 3s COURTS")
+OldTowns = Room("NBA 2K COURTS","basketball_courts","NBA 2K DOTS","NBA 2K 3s COURTS","clothes_store")
+Sunset_Beach = Room("NBA 2K COURTS","NBA 2K 3s COURTS","NBA 2K 2s COURTS","clothes_store")
+Rivet_City = Room("NBA 2K COURTS","NBA 2K 3s COURTS","NBA 2K 2s COURTS","clothes_store")
+MyCourt = Room("basketball_courts","MyCourt couch","MyCourt tv","MyCourt window")
+JordanRecCenter = Room("NBA 2K 3s COURTS","RecCenter_COURT","JordanRecCenter_Ref")
+
 
 player = Player(courts)
 
