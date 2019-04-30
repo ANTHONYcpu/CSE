b3c13c97fdc1575b2c202e7a45036755d851e1d2
ANTHONYcpu <44374206+ANTHONYcpu@users.noreply.github.com>
2019-04-30 09:43:52 -0700

    validator

--- Anthony W Game.py
+++ Anthony W Game.py
@@ -1,9 +1,10 @@
 class Room(object):
-    def __init__(self, name, description, north=None, south=None, east=None):
+    def __init__(self, name, description, north=None, south=None, east=None, west=None):
         self.name = name
         self.north = north
         self.south = south
         self.east = east
+        self.west = west
         self.description = description
 
 
---
+++ Anthony W Validator.py
@@ -0,0 +1,8 @@
+def valid_card_number(num: str):
+    ???????
+
+print(valid_card_number("8056292510703970"))
+
+list_num = list(number)
+for index in range(len(list_num)):
+    list_num[index] = int(list_num[index])
