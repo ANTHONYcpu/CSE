661c5b70c8fd9e4e4151cf11275e2d6857fd4142
ANTHONYcpu <44374206+ANTHONYcpu@users.noreply.github.com>
2019-05-14 09:48:19 -0700

    salesinfo

--- SalesInfo.py
+++ SalesInfo.py
@@ -1,9 +1,8 @@
 import csv
 
-
 with open("Sales Records.csv") as old_csv:
     reader = csv.reader(old_csv)
     for row in reader:
 
-        old_number = row[0]
+        old_number = row[13]
         print(old_number)
